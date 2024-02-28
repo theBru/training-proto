2e4551f478260fb7be46f41b6f5f9b495212a5e0
theBru <gsarbanoglu@gmail.com>
2024-02-28 15:31:03 +0000

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// This is routing from do you have a pet question
+router.post('/what-is-your-pets-name', function(request, response) {
+    var petResponse = request.session.data['have-pet']
+    if (petResponse == "No") {
+        response.redirect('/check-your-answers')
+    } else {
+        response.redirect('what-is-your-pets-name')
+    }
+})
+
+// *********
+// This is for another routing
+// *********
